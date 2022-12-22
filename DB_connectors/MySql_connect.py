cf4c50f2c4069da986105ab150fd31f3023740dc
Fugguri <fygguri@icloud.com>
2022-12-22 13:39:38 +0300

    first commit

--- DB_connectors/MySql_connect.py
+++ DB_connectors/MySql_connect.py
@@ -265,7 +265,8 @@ class Database:
                 "SELECT id FROM users WHERE telegram_id = (%s)", (telegram_id,))
             user_id = cursor.fetchone()[0]
             cursor.execute(
-                "SELECT id FROM chats WHERE chat LIKE '%s'", (chat,))
+                "SELECT id FROM chats WHERE chat LIKE (%s)", (chat,))
+            print(chat)
             chat_id = cursor.fetchone()[0]
             cursor.execute(
                 'DELETE FROM users_chats WHERE user_id = %s AND chat_id =%s', (user_id, chat_id))
